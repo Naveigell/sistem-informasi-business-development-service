add8884ae4eb9590948ce474599764b7e6a1b8c9
Naveigell <yosua.yerikho123@gmail.com>
2022-08-25 21:05:40 +0800

    change admin login page title

--- app/Views/auth/admin/login.php
+++ app/Views/auth/admin/login.php
@@ -1,7 +1,7 @@
 <html lang="en"><head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>Login — Stisla</title>
+    <title>Login Admin</title>
 
     <!-- General CSS Files -->
     <?= $this->include('layouts/admin/style'); ?>
